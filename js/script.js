f700a4b4412da7e3224740f7d5c917c34929ae49
yusuf <yusuffdemirci10@gmail.com>
2023-03-14 02:04:30 +0300

    responsive design

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const menuBtn = document.querySelector("#menu-btn");
+const navbar = document.querySelector(".navbar");
+
+console.log(navbar);
+console.log(menuBtn);
+
+menuBtn.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  document.addEventListener("click", function (e) {
+    if (
+      !e.composedPath().includes(menuBtn) &&
+      !e.composedPath().includes(navbar)
+    ) {
+      navbar.classList.remove("active");
+    }
+  });
+});
